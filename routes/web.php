a526560bb0ba2af7bffde5106985cdedabfa2e73
Lena Lehmann <smilie79@web.de>
2020-07-13 13:00:10 +0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', 'HomeController@index')->name('index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('home', 'HomeController@index')->name('home');
 
-
-Route::post('/kalender', 'EventController@store')->name('event_create');
-Route::put('/kalender', 'EventController@edit')->name('event_edit');
-Route::get('/getEvents', 'EventController@show');
-
+// KALENDER //
+Route::get('getEvents', 'EventController@index');
+Route::post('newEvent', 'EventController@store')->name('event_store');
