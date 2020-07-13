27b8a406b54946833b20c7ac8f4110be0462e636
Lena Lehmann <smilie79@web.de>
2020-07-13 13:01:19 +0200

    Add files via upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,13 @@
     <main id="main" class="container-fluid">
         <div class="calWrapper">
             <h1>TERMINKALENDER</h1>
-            <div class="w-11/12 mx-auto mt-4">
-                <div id="calendar"></div>
-                @include('calendar.createEvent')
-            </div>
+            {{--
+            <form class="w-11/12 mx-auto mt-4" method="post" action="{{ route('event_store') }}">
+                @csrf
+                <calendar-component></calendar-component>
+            </form>
+            --}}
+            <calendar-component></calendar-component>
         </div>
     </main>
 @endsection
